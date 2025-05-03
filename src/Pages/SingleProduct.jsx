9911ab2b9bb1abca2e61ceeae405721f0725a4b2
RaoEzan <162316384+RaoEzan@users.noreply.github.com>
2025-05-04 00:40:44 +0500

    Update SingleProduct.jsx

--- src/Pages/SingleProduct.jsx
+++ src/Pages/SingleProduct.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import useFetch from '../hooks/useFetch'
+import useFetch from '../Hooks/useFetch'
 
 const SingleProduct = () => {
     const params = useParams()
